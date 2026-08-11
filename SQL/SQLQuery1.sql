88807a36ddb344cc08897aca6b743b91d34c0817
DEV CHAUHAN <166748796+DEVCHAUHAN2004@users.noreply.github.com>
2026-08-11 21:09:30 +0530

    Add files via upload

---
+++ SQL/SQLQuery1.sql
@@ -0,0 +1,69 @@
+--select * from EmployeeRecords
+
+--select * from EmployeeRecords
+--where Salary >= 70000
+
+--SELECT * 
+--FROM EmployeeRecords
+--WHERE Department = 'HR'
+
+--SELECT * FROM EmployeeRecords
+--WHERE FirstName LIKE 'J%'
+
+--SELECT * FROM EmployeeRecords
+--ORDER BY FirstName
+
+--SELECT * FROM EmployeeRecords
+--WHERE Salary >= 75000
+--ORDER BY Salary
+
+--SELECT * FROM EmployeeRecords
+--WHERE Salary >= 75000
+--ORDER BY Salary DESC
+
+
+--SELECT * FROM EmployeeRecords
+--WHERE Salary>= 75000 AND Department = 'IT'
+
+--SELECT * FROM EmployeeRecords
+--WHERE Salary >= 75000 OR Department = 'Finance'
+--ORDER BY SALARY
+
+--SELECT * FROM EmployeeRecords
+--WHERE Salary BETWEEN 75000 AND 80000
+
+--SELECT * FROM EmployeeRecords
+--WHERE Salary NOT BETWEEN 75000 AND 80000
+
+
+--SELECT * FROM EmployeeRecords
+--WHERE Department IN ('HR','IT')
+
+--SELECT * FROM Employees
+--INSERT INTO Employees(EmployeeID,FirstName,LastName,Department,Salary,HireDate)
+--VALUES(6,'Dev','Chauhan','DS',90000,'2025-08-16')
+
+--SELECT * FROM Employees
+--WHERE Department IS NOT NULL
+
+--UPDATE Employees
+--SET FirstName = 'Ram'
+--WHERE EmployeeID = 6
+
+--SELECT * FROM Employees
+
+--DELETE FROM Employees
+--WHERE EmployeeID = 6
+
+
+
+--SELECT * FROM EmployeeRecords
+--TRUNCATE TABLE EmployeeRecords
+
+--select * from EmployeeRecords
+
+--drop table EmployeeRecords
+
+--SELECT TOP 2 * FROM Employees
+--ORDER BY Salary DESC
+
